b134f14d4e21ba1f8d486d9a190bbbcaa202e12f
mr9bit <a.mazaew@lambda-it.ru>
2019-04-11 23:56:41 +0300

    first commit

---
+++ policy.py
@@ -0,0 +1,7 @@
+from spinup.utils.test_policy import load_policy, run_policy
+import rocket_lander_gym
+import gym
+
+_, get_action = load_policy('oupput')
+env = gym.make('RocketLander-v0')
+run_policy(env, get_action)
