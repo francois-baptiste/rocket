a1cc436da8c67ec014080101525927dd4e92368f
baptiste <XSER543z>
2019-11-27 15:11:04 +0100

    hardlink submodules

--- agent_learning.py
+++ agent_learning.py
@@ -1,11 +1,12 @@
-from spinup import ppo
+from spinup.algos.ppo.ppo import ppo
 import tensorflow as tf
 import gym
 import rocket_lander_gym
-env_fn = lambda : gym.make('RocketLander-v0')
+def env_fn():
+    return gym.make('RocketLander-v0')
 
 ac_kwargs = dict(hidden_sizes=[64, 64], activation=tf.nn.relu)
 
-logger_kwargs = dict(output_dir='oupput/', exp_name='experiment_name')
+logger_kwargs = dict(output_dir='output/', exp_name='experiment_name')
 
 ppo(env_fn=env_fn, ac_kwargs=ac_kwargs, steps_per_epoch=5000, epochs=2000, logger_kwargs=logger_kwargs)
--- agent_testing.py
+++ agent_testing.py
@@ -2,6 +2,6 @@ from spinup.utils.test_policy import load_policy, run_policy
 import rocket_lander_gym
 import gym
 
-_, get_action = load_policy('oupput')
+_, get_action = load_policy('output')
 env = gym.make('RocketLander-v0')
 run_policy(env, get_action)
